5d5aadf1ad2094ab3e961b0952d6f6ee814b1ba2
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-19 21:56:21 +0200

    Erweitere Team-App um Postfach, Kalender und Materialverwaltung

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "arbeitszeit-team-v1";
+const CACHE_NAME = "arbeitszeit-team-v2";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=5",
+  "./app.js?v=6",
   "./config.js?v=1",
   "./manifest.webmanifest",
   "./icon.svg",

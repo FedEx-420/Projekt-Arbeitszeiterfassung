0ece3183085ece16811b2ccddbf33057bcad3a07
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 01:01:41 +0200

    App-Cache für Abwesenheitsverwaltung aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "arbeitszeit-team-v11";
+const CACHE_NAME = "arbeitszeit-team-v12";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=15",
+  "./app.js?v=16",
   "./config.js?v=1",
   "./manifest.webmanifest",
   "./icon.svg",

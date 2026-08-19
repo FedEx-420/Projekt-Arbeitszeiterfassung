4c1def384b7de0d16d2864c23e738d8aecb5b02b
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-19 03:13:46 +0200

    Team-Arbeitszeit-App aktualisieren

---
+++ config.js
@@ -0,0 +1,7 @@
+// This is a public project key. Access to all working-time data is enforced
+// by login sessions and database rules; no administrator secret is present here.
+window.WORKTIME_CONFIG = {
+  supabaseUrl: 'https://yzlnfrljlknazywskylb.supabase.co',
+  supabasePublishableKey: 'sb_publishable_NjNYJRgtzol7JARAuzZ1Bg_gaSrY0OZ',
+};
+

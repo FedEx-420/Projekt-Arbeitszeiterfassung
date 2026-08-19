377320239838e137399885739856972b0d349724
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 01:41:04 +0200

    Cache für Urlaubsanträge erneuern

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "arbeitszeit-team-v19";
+const CACHE_NAME = "arbeitszeit-team-v20";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=19",
+  "./app.js?v=20",
   "./config.js?v=1",
   "./manifest.webmanifest",
   "./icon.svg",

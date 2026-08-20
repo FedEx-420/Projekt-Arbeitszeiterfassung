32c28e1efdd3850941d64d27bb9715974b02a6d3
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 19:29:16 +0200

    App-Cache Version 202 aktivieren

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'arbeitszeit-rebuild-v201'
-const FILES = ['./', './index.html', './styles-rebuild.css?v=201', './app-rebuild.js?v=201', './config.js?v=1', './manifest.webmanifest', './icon.svg']
+const CACHE = 'arbeitszeit-rebuild-v202'
+const FILES = ['./', './index.html', './styles-rebuild.css?v=202', './app-rebuild.js?v=202', './config.js?v=1', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)))

d036108c5b969b641e61825bfac912185a88a88f
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 02:12:06 +0200

    Refresh immediate interaction update cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "arbeitszeit-team-v21";
+const CACHE_NAME = "arbeitszeit-team-v22";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=21",
+  "./app.js?v=22",
   "./config.js?v=1",
   "./manifest.webmanifest",
   "./icon.svg",

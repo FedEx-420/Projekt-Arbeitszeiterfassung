9381b6048aa139b996c273294316e1ee80b32b7d
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 20:54:37 +0200

    Neue Konten- und Lohnabrechnungsansicht veröffentlichen

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'arbeitszeit-rebuild-v301'
-const FILES = ['./', './index.html', './styles-rebuild.css?v=301', './app-rebuild.js?v=301', './config.js?v=1', './manifest.webmanifest', './icon.svg']
+const CACHE = 'arbeitszeit-rebuild-v400'
+const FILES = ['./', './index.html', './styles-rebuild.css?v=400', './app-rebuild.js?v=400', './config.js?v=1', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)))

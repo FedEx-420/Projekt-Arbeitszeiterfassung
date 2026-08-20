c9fae393284e2f90f782927e201850fe2e192564
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-20 19:00:30 +0200

    Aktualisiere App-Cache auf Version 107

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "arbeitszeit-neu-v106";
-const APP_FILES = ["./", "./index.html", "./styles-new.css?v=103", "./styles-week.css?v=103", "./styles-104.css?v=104", "./app-new.js?v=106", "./config.js?v=1", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "arbeitszeit-neu-v107";
+const APP_FILES = ["./", "./index.html", "./styles-new.css?v=103", "./styles-week.css?v=103", "./styles-104.css?v=104", "./app-new.js?v=107", "./config.js?v=1", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener("fetch", event => { if (event.request.method !== "GET") return; event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy)); return response; }).catch(() => caches.match(event.request))); });

39a629a60993ad20d47771cc6672b7722159e7d7
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-22 00:33:22 +0200

    Refresh company branding cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'arbeitszeit-rebuild-v404'
-const FILES = ['./', './index.html', './styles-rebuild.css?v=404', './app-rebuild.js?v=404', './config.js?v=1', './manifest.webmanifest', './icon.svg']
+const CACHE = 'arbeitszeit-rebuild-v405'
+const FILES = ['./', './index.html', './styles-rebuild.css?v=405', './app-rebuild.js?v=405', './config.js?v=1', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)))

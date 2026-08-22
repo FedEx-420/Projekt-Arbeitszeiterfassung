2e72ccf0c30cb09891af180a1b11207d62cc9950
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-22 03:54:16 +0200

    Aktualisierte Abwesenheitsansicht zwischenspeichern

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'arbeitszeit-neu-v602';
-const FILES = ['./','./index.html','./styles-v601.css','./app-v602.js','./config.js?v=600','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'arbeitszeit-neu-v603';
+const FILES = ['./','./index.html','./styles-v601.css','./app-v603.js','./config.js?v=600','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request).then(response => { if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone())); return response; }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))); });

67eebe23da773ea376c992c6e14b6fa92b5f15ea
FedEx-420 <qbx9wq8s8j@privaterelay.appleid.com>
2026-08-22 18:22:52 +0200

    Refresh vacation allowance cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'arbeitszeit-neu-v731';
-const FILES = ['./','./index.html','./styles-v700.css','./responsive-v701.css','./navigation-v710.css','./menu-v720.css','./app-v700.js?v=731','./config.js?v=600','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'arbeitszeit-neu-v733';
+const FILES = ['./','./index.html','./styles-v700.css','./responsive-v701.css','./navigation-v710.css','./menu-v720.css','./app-v700.js?v=733','./config.js?v=600','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request).then(response => { if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone())); return response; }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))); });
